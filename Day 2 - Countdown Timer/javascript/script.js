78fe9427c99a9ba9dbfb7d61a3fe2f012ef0dfdf
Aphelion-im <slaygen@gmail.com>
2021-01-12 23:43:40 +0100

    Added monotype font for the jumping numbers

--- Day 2 - Countdown Timer/javascript/script.js
+++ Day 2 - Countdown Timer/javascript/script.js
@@ -3,27 +3,31 @@
 window.addEventListener("load", () => {
 
 
-const countDownDate = new Date("Dec 31, 2030 00:00:00").getTime();
+    const countDownDate = new Date("Dec 31, 2030 00:00:00").getTime();
 
-const x = setInterval( () => {
+    const x = setInterval(() => {
 
-    const now = new Date().getTime();
+        const now = new Date().getTime();
 
-    const distance = countDownDate - now;
+        const distance = countDownDate - now;
 
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.querySelector("#output").innerHTML = `<span class='days'>${days} days<span><span class="hours">${hours} hours</span><span class="minutes">${minutes} minutes</span><span class="seconds">${seconds} seconds</span>`;
+        document.querySelector("#output").innerHTML = `<span class='days'>${days} days<span><span class="hours">${hours} hours</span><span class="minutes">${minutes} minutes</span><span class="seconds">${seconds} seconds</span>`;
 
 
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("output").innerHTML = "OFFER EXPIRED";
-    }
-}, 1000);
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("output").innerHTML = "OFFER EXPIRED";
+        }
+    }, 1000);
 
 
 }); // End load eventlistener
+
+/* Issues:
+Timer jumps when below 10
+*/
