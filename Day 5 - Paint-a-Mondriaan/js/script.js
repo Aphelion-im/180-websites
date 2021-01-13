d9689bc60d204c41bbb9db61905277ede8241f18
Aphelion-im <slaygen@gmail.com>
2021-01-14 00:15:42 +0100

    Repaired Paint-a-Mondrian

---
+++ Day 5 - Paint-a-Mondriaan/js/script.js
@@ -0,0 +1,86 @@
+/* Based on and inspired by Jennifer Dewalt's "Paint a Mondrian" (Day 4). https://jenniferdewalt.com
+
+Created by André de Groot.
+*/
+"use strict";
+
+window.addEventListener("load", () => {
+
+    // variables
+    const artistName = document.querySelector("#artistNameInput");
+    const outputArtistName = document.querySelector(".outputArtistName");
+    let paintColor = "white";
+    //Paint splatters
+    const redPaint = document.querySelector(".redPaint");
+    const bluePaint = document.querySelector(".bluePaint");
+    const whitePaint = document.querySelector(".whitePaint");
+    const yellowPaint = document.querySelector(".yellowPaint");
+    const blackPaint = document.querySelector(".blackPaint");
+    const painting = document.querySelector(".painting");
+
+    //Enter artistname, handwriting font. On/Off.
+    artistName.addEventListener("input", () => {
+
+            outputArtistName.textContent = artistName.value;
+        }
+
+    );
+
+
+
+
+    // Paint splatters. (Refactor this code!)
+    redPaint.addEventListener("click", () => {
+
+        paintColor = "red";
+        return paintColor;
+
+    });
+
+    bluePaint.addEventListener("click", () => {
+
+        paintColor = "blue";
+        return paintColor;
+
+    });
+
+    yellowPaint.addEventListener("click", () => {
+
+        paintColor = "yellow";
+        return paintColor;
+
+    });
+
+    whitePaint.addEventListener("click", () => {
+
+        paintColor = "white";
+        return paintColor;
+
+    });
+
+    blackPaint.addEventListener("click", () => {
+
+        paintColor = "black";
+        return paintColor;
+
+    });
+
+
+    //Boxes
+    painting.addEventListener("click", (event) => {
+
+        if (event.target.className === "painting") {
+            // Do nothing!
+        } else {
+            event.target.style.backgroundColor = paintColor;
+        }
+    });
+
+
+
+}); // End load eventlistener
+
+
+// Generate random canvas
+// Export canvas to picture + artist name bottom right. (.jpg, .png)
+// Generate random blocks
