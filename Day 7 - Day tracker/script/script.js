82258a701d52aa8a8f902c0e10c380544f9e6e53
Aphelion-im <slaygen@gmail.com>
2021-01-16 02:17:52 +0100

    Added expanded vertical bar from main bar when a certain day is active

--- Day 7 - Day tracker/script/script.js
+++ Day 7 - Day tracker/script/script.js
@@ -4,34 +4,40 @@ window.addEventListener("load", () => {
 
   const date = new Date;
 
-  let dayArray = [
-    {
+  let dayArray = [{
+      id: 0,
       day: "Sunday",
-      message: "Going to church today?"
+      message: "It is Sunday. Going to church today?!"
     },
     {
+      id: 1,
       day: "Monday",
-      message: "Back to work!"
+      message: "It is Monday. Back to work!"
     },
     {
+      id: 2,
       day: "Tuesday",
-      message: "Taking it slow today"
+      message: "It is Tuesday. Taking it slow today!"
     },
     {
+      id: 3,
       day: "Wednesday",
-      message: "Hlafway there!"
-    },    
+      message: "It is wednesday. Halfway there!"
+    },
     {
+      id: 4,
       day: "Thursday",
-      message: "Only one day to go!"
+      message: "It is Thursday. Only one day to go!"
     },
     {
+      id: 5,
       day: "Friday",
-      message: "Almost there! Just a few hours"
+      message: "It is Friday. Almost there! Just a few hours!"
     },
     {
+      id: 6,
       day: "Saturday",
-      message: "Finally free! Party time"
+      message: "It is Saturday. Finally free! Party time!"
     }
   ];
 
@@ -39,6 +45,35 @@ window.addEventListener("load", () => {
   const message = document.querySelector(".message");
 
   message.innerHTML = `${dayArray[day].message}`;
+
+
+  // Get value from tabs
+
+  let dayTabs = document.querySelectorAll(".tab");
+
+  for (let dayTab of dayTabs) {
+
+    if ((dayTab.getAttribute("data-day")) == dayArray[day].id) {
+      dayTab.style.backgroundColor = "red";
+      dayTab.classList.remove("tab");
+      dayTab.classList.add("today");
+
+    }
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
