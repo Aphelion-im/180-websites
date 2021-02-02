723dc05dfe8503ccc03984570980ef0d2ee54350
Aphelion-im <aphelion-im@protonmail.com>
2021-02-02 03:10:33 +0100

    Added winning combinations Tic Tac Toe

--- Day 15 - Tic Tac Toe (x)/README.md
+++ Day 15 - Tic Tac Toe (x)/README.md
@@ -1,16 +1,19 @@
 # Tic Tac Toe
 
-Updated: 31-1-2021
+Updated: 2-2-2021
 
 * console.clear();
 * forEach(circle => { circle.expressions });
 * children.remove;
-* removeEventListener();
 * circle.classList.remove("red", "blue");
 * circle.classList.contains("red");
 * .textContent = "";
+* Disable annoying popups within VSC when you want to select something. Settings > Search > Hover > Hover Delay (1500ms) & Hover enabled.
+* 9 circles in Nodelist: circles[winningCombinations[y][2]]; --> [winningCombinations[0][2] --> 3 --> circles[3];
 * Some ideas from:
 https://www.thecrazyprogrammer.com/2019/09/javascript-tic-tac-toe-game.html
+* https://youtu.be/dBlSiGOFjUY  
 
 ## To do
-* Check for winner. Compares red/blue matrix with winning numbers.
+* noone wins
+* cannot overwrite choice
--- Day 15 - Tic Tac Toe (x)/script/script.js
+++ Day 15 - Tic Tac Toe (x)/script/script.js
@@ -1,3 +1,5 @@
+/* Last update 2-2-2021 */
+
 "use strict";
 
 window.addEventListener("load", () => {
@@ -6,127 +8,62 @@ window.addEventListener("load", () => {
   const board = document.querySelector(".board");
   const output = document.querySelector(".output");
   const button = document.querySelector("button");
-  const winningCombinations = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [1, 5, 9],
-    [3, 5, 7],
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 6, 9]
-  ];
-
-  const redArray = [];
-  const blueArray = [];
-  const numberOfCircles = 9;
+  const circles = document.querySelectorAll(".circle");
   let counter = true;
 
-  output.textContent = "Red's turn"; // Standard message
-  createBoard();
-
-  function createBoard() {
-    for (let i = 1; i <= numberOfCircles; i++) {
-      const circle = document.createElement("div");
-      circle.setAttribute("class", "circle");
-      circle.setAttribute("data-item", i);
-      circle.addEventListener("click", paintCircle);
-      board.appendChild(circle);
+  function paintCircle() {
+    if (counter) {
+      this.classList.add("red");
+      output.textContent = "Blue player's turn!";
+      counter = false;
+    } else {
+      this.classList.add("blue");
+      output.textContent = "Red player's turn!";
+      counter = true;
     }
+    checkWinningCombinations();
   }
 
-  function paintCircle(e) {
 
-    const dataItem = e.target.getAttribute("data-item");
+  function checkWinningCombinations() {
 
-    if (counter) {
-      e.target.classList.add("red");
-      output.textContent = "Blue's turn";
-      counter = false;
-      redArray.push(dataItem);
-    } else {
-      e.target.classList.add("blue");
-      output.textContent = "Red's turn";
-      counter = true;
-      blueArray.push(dataItem);
+    const winningCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+      [0, 3, 6],
+      [0, 4, 8],
+      [2, 5, 8],
+    ];
+
+    for (let y = 0; y < winningCombinations.length; y++) {
+      const square1 = circles[winningCombinations[y][0]];
+      const square2 = circles[winningCombinations[y][1]];
+      const square3 = circles[winningCombinations[y][2]];
+
+      if (square1.classList.contains('red') && square2.classList.contains('red') && square3.classList.contains('red')) {
+        output.textContent = 'Red player wins!';
+      } else if (square1.classList.contains('blue') && square2.classList.contains('blue') && square3.classList.contains('blue')) {
+        output.textContent = 'Blue player wins!';
+      }
     }
-    paintColor();
   }
 
   // Reset board
   button.addEventListener("click", () => {
     console.clear();
     board.children.remove; // Destroy board and draw again
-    const circles = document.querySelectorAll(".circle");
     for (let circle of circles) {
       circle.addEventListener("click", paintCircle);
       circle.classList.remove("red", "blue");
-      circle.classList.remove("crsr");
       counter = true;
-      output.textContent = "Red's turn";
-      redArray.length = "";
-      blueArray.length = "";
+      output.textContent = "Red player's turn";
     }
   });
 
-
-  function paintColor() {
-
-    console.clear();
-
-    const circles = document.querySelectorAll(".circle");
-
-    circles.forEach(circle => {
-
-      // After painting a color remove the event listener and change the cursor to default
-      if (circle.classList.contains("red") || circle.classList.contains("blue")) {
-        circle.removeEventListener("click", paintCircle);
-        circle.classList.add("crsr");
-      }
-
-    });
-    countColors();
-    // checkWinner();
-  }
-
-  function countColors() {
-
-    const totalArray = redArray.length + blueArray.length;
-
-    if (totalArray >= 9) {
-      output.textContent = "No more moves available!";
-    }
-
-  }
-
-//   function checkWinner() {
-//     let strRed = redArray;
-//     let strBlue = blueArray;
-//     let strWinningCombinations = winningCombinations;
-
-//     console.log(strRed);
-//     console.log(strBlue);
-//     console.log(strWinningCombinations);
-
-// for (let combo of strWinningCombinations) {
-
-//       if (strWinningCombinations.includes(combo, 0)) {
-//         output.textContent = "Red is winner!";
-
-
-//       } else if (strWinningCombinations.includes(combo, 0)) {
-//         output.textContent = "Blue is winner!";
-//       }
-//     }
-
-
-//   }
-
-
-
-
-
-
-
+  // Attach eventlisteners to each empty circle
+  circles.forEach(circle => circle.addEventListener("click", paintCircle));
 
 }); // Load eventlistener 
