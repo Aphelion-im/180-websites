36914f0b66061041b9e9a55b9c2a8870c0e52a30
Aphelion-im <aphelion-im@protonmail.com>
2021-02-07 02:40:43 +0100

    Improved Day 17 - Memory JS. Now includes pictures

--- Day 17 - Memory JS/README.md
+++ Day 17 - Memory JS/README.md
@@ -2,9 +2,11 @@
 
 * 72 cards (36 pairs)
 * :root {--blue: #AEDCE9; } --> background-color: var(--blue);
-* 
+* .firstElementChild(); (Similar to .firstChild());
+
 
 
 
 ## To do
 * Turn 2 cards at a time. If both cards are not equal turn blue again.
+* cards not allowed to click twice to score.
--- Day 17 - Memory JS/scripts/script.js
+++ Day 17 - Memory JS/scripts/script.js
@@ -2,68 +2,123 @@
 
 window.addEventListener("load", () => {
 
+  const cardsArray = [{
+      image: "../images/cards/card-front-apple-200x200.jpg",
+      name: "Apple",
+      alt: "Apple card"
+    },
+    {
+      image: "../images/cards/card-front-banana-200x200.jpg",
+      name: "Banana",
+      alt: "Banana card"
+    },
+    {
+      location: "../images/cards/card-front-beetle-200x200.jpg",
+      name: "Beetle",
+      alt: "Beetle card"
+    }
+  ];
+
+
+
   // Variables
-  const grid = document.querySelector(".grid");
   const cards = document.querySelectorAll(".card");
-  const output = document.querySelector(".output");
+  const scoreOutput = document.querySelector(".output");
+  const flipsOutput = document.querySelector(".flips");
+  const btn_newGame = document.querySelector(".btn_newGame");
+  let chosenArray = [];
+
   let score = 0;
-  const imagesDir = "../images/";
-  const cardsArray = ["1", "2", "3", "4", "5", "6", "1", "2", "3", "4", "5", "6"];
-  let chosenCards = [];
+  let flips = 0;
+  let clicks = 0;
+
 
   // Eventlisteners
   cards.forEach(card => card.addEventListener("click", turnCard));
-
-  // Array
-
+  btn_newGame.addEventListener("click", newGame);
 
 
-  // for (let i = 0; i < cardsArray.length; i++) {
-  //   for (let card of cards) {
-  //     card.textContent = cardsArray[i];
-  //   }
-  // }
+  newGame();
 
 
 
 
+  function turnCard(e) {
 
-  // Setup
-  output.textContent = `Score: ${score}`;
-
-
-
-
-  function turnCard() {
-    console.log("Turn card!");
-    this.style.background = "none";
-    this.style.color = "darkslategray";
-    let chosenCard = this.textContent;
     console.clear();
-    console.log("Card chosen: " + chosenCard);
-    chosenCards.push(chosenCard);
-    console.log("Chosen cards: " + chosenCards);
+    flips++;
+    clicks++;
+    flipsOutput.textContent = `Flips: ${flips}`;
 
-    if (chosenCards[0] == chosenCards[1]) {
-      console.log("Equal");
-      chosenCards.splice(0, 2);
-      console.log(chosenCards);
-      score++;
-      output.textContent = `Score: ${score}`;
+    // Turn card. Show content of card. Remove background.
+    this.style.background = "none";
+    this.firstElementChild.style.opacity = 100;
+
+    let choice = e.target.getAttribute("data-card");
+    e.target.setAttribute("data-counter", 1);
+
+
+    if (e.target.getAttribute("data-counter") == "1") {
+      console.log("No more clicking allowed");
+      this.removeEventListener("click", turnCard);
+      e.target.style.cursor = "default";
+    } 
+
+
+
+
+    putBackCard();
+  };
+
+
+
+
+
+
+  function checkMatch() {
+    const dataCard = card.firstElementChild.getAttribute("data-card");
+    if (dataCard == chosenArray[0] || dataCard == chosenArray[1]) {
+      console.log("Match!");
     } else {
-      console.log("Not equal");
-      setTimeout(() => {
-        this.style.backgroundImage = `url(${imagesDir}card-back-200x200-v2.png)`;;
-        this.style.color = `rgba(0, 0, 0, 0)`;
-      }, 1000);
+      console.log("No match!");
+      putBackCard();
     }
-
-
 
   }
 
 
 
+  // Turn the card back again with blue side facing up
+  function putBackCard() {
+    for (let card of cards) {
+      setTimeout(() => {
+        card.removeAttribute("style");
+        card.firstElementChild.style.opacity = 0;
+        card.style.backgroundImage = `url(../images/card-back-200x200-v2.png)`;
+        card.addEventListener("click", turnCard);
+        card.style.cursor = "pointer";
+      }, 2000);
+    };
+  }
+
+  // Start a new game. Reset all stats and shuffle the cards.
+  function newGame() {
+    cards.forEach(card => card.firstElementChild.style.opacity = 0);
+    cards.forEach(card => card.style.backgroundImage = `url(../images/card-back-200x200-v2.png)`);
+    shuffle();
+    flips = 0;
+    score = 0;
+    scoreOutput.textContent = `Score: ${score}`;
+    flipsOutput.textContent = `Flips: ${flips}`;
+  }
+
+  // Shuffle the cards
+  function shuffle() {
+    for (let card of cards) {
+      let num = Math.floor(Math.random() * cards.length);
+      card.style.order = num;
+    }
+  }
 
 
 
