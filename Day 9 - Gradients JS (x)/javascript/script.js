1ede84339e43a5346b375a021e14cf3e38041d20
Aphelion-im <aphelion-im@protonmail.com>
2022-11-06 16:27:14 +0100

    Deleted some stuff

--- Day 9 - Gradients JS (x)/javascript/script.js
+++ Day 9 - Gradients JS (x)/javascript/script.js
@@ -6,28 +6,28 @@ window.addEventListener('load', function () {
   const kleur1 = document.querySelector('#colorPicker1');
   const kleur2 = document.querySelector('#colorPicker2');
   const output = document.querySelector('#output');
-  const copypaste = document.querySelector('#copypaste');
+  const copypaste = document.querySelector('.fa-copy');
 
   const achtergrond = document.querySelector('body');
 
 
-  kleur1.addEventListener('change', function () {
+  kleur1.addEventListener('input', function () {
 
-    achtergrond.style.backgroundImage = 'linear-gradient(' + kleur1.value + ',' + kleur2.value + ')';
-    output.innerHTML = '<code>background: linear-gradient(' + kleur1.value + ', ' + kleur2.value + ');</code>';
+    achtergrond.style.backgroundImage = `linear-gradient(${kleur1.value}, ${kleur2.value})`;
+    output.textContent = `background: linear-gradient(${kleur1.value}, ${kleur2.value});`;
 
   });
 
-  kleur2.addEventListener('change', function () {
+  kleur2.addEventListener('input', function () {
 
-    achtergrond.style.backgroundImage = 'linear-gradient(' + kleur1.value + ',' + kleur2.value + ')';
-    output.innerHTML = '<code>background: linear-gradient(' + kleur1.value + ', ' + kleur2.value + ');</code>';
+    achtergrond.style.backgroundImage = `linear-gradient(${kleur1.value}, ${kleur2.value})`;
+    output.innerHTML = `<code>background: linear-gradient(${kleur1.value}, ${kleur2.value});</code>`;
 
   });
 
   copypaste.addEventListener('click', function () {
     // Copy to clipboard
-    navigator.clipboard.writeText('background: linear-gradient(' + kleur1.value + ', ' + kleur2.value + ');');
+    navigator.clipboard.writeText(`background: linear-gradient(${kleur1.value}, ${kleur2.value});`);
     console.log('Copied to clipboard!');
   });
 
